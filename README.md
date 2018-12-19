0b149f173ed762ad7940fecf0e10cb87f1333cbd
mop <duanpanzi@163.com>
2018-12-19 21:07:28 +0800

    更新至iOS12.1.2、macOS Mojave

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ios-emoji-webpng
 搜狗iOS系统表情跨平台展示解决文案
 
-   大概在两年前，在项目中遇到了"用户在iPhone手机上输入iOS系统自带表情时，输入的emoji code在windows浏览器等其他平台下会显示为乱码"的问题，对于UGC内容的显示非常不友好，通过进一步分析发现，不同iOS版本下 emoji code编码规则也比较繁杂，为了解决这一问题，问问在实际项目中探索实现了一套比较完整的将iOS自带emoji code转换为web png跨平台展示的方法，最近iOS和macOS相继发布了新版本，因此现在总结开放出来，希望对大家有帮助，目前已经支持iOS10.2.1、macOS Sierra。
+   大概在两年前，在项目中遇到了"用户在iPhone手机上输入iOS系统自带表情时，输入的emoji code在windows浏览器等其他平台下会显示为乱码"的问题，对于UGC内容的显示非常不友好，通过进一步分析发现，不同iOS版本下 emoji code编码规则也比较繁杂，为了解决这一问题，问问在实际项目中探索实现了一套比较完整的将iOS自带emoji code转换为web png跨平台展示的方法，最近iOS和macOS相继发布了新版本，因此现在总结开放出来，希望对大家有帮助，目前已经支持iOS12.1.2、macOS Mojave
 
 ##获取emoji code与emoji web png图片文件之间的映射关系表
 将代码下载至本地：
@@ -10,18 +10,14 @@
 
 
 进入ios-emoji-webpng目录执行：
-    
-    unzip emoji_png.zip
-    
-> emoji_png.zip包中的表情图片文件是基于[github/gemoji](https://github.com/github/gemoji/)，多肤色表情支持基于[s0meone/gemoji](https://github.com/s0meone/gemoji)来导出，非常感谢这两个项目带来的启示！在iOS和macOS后续升级中大家后续可以关注这两个项目。
-
-解压图片完成后再执行：
 
     node ./parse.js
     
+> emoji_png.zip包中的表情图片文件是基于[github/gemoji](https://github.com/github/gemoji/)，多肤色表情支持基于[s0meone/gemoji](https://github.com/s0meone/gemoji)来导出，非常感谢这两个项目带来的启示！在iOS和macOS后续升级中大家后续可以关注这两个项目。
+    
 如果执行成功会看到如下结果：
 
-    已成功处理表情文件: 1761个
+    已成功处理表情文件: 1872个
     
 同时会在当前目录下生成一个map.json文件，这个JSON文件包含了emoji code与emoji png格式图片文件名之间的映射关系。
 
--- parse.js
+++ parse.js
@@ -1,25 +1,25 @@
-var convertUTF32MToUTF16m = require('./lib/convert').convertUTF32MToUTF16m;
-var fs = require('fs');
-var path = require('path');
+const convertUTF32MToUTF16m = require('./lib/convert').convertUTF32MToUTF16m;
+const fs = require('fs');
+const path = require('path');
 
 (function (dir) {
-    var emojiPNGFileNameList = {};
-    var emojiMapResult = {};
-    var fileList = fs.readdirSync(dir);
+    const emojiPNGFileNameList = {};
+    const emojiMapResult = {};
+    const fileList = fs.readdirSync(dir);
     //性别标识值
-    var isSexNumberStr = /2640|2642/;
-    var totalCount = 0;
+    const isSexNumberStr = /2640|2642/;
+    let totalCount = 0;
 
-    for (var i = 0; i < fileList.length; i++) {
-        var filePath = path.join(dir, fileList[i]);
-        var stat = fs.statSync(filePath);
+    for (let i = 0; i < fileList.length; i++) {
+        const filePath = path.join(dir, fileList[i]);
+        const stat = fs.statSync(filePath);
 
         if (stat.isFile() && filePath.indexOf('.png') > -1) {
-            var basename = path.basename(filePath, '.png');
+            const basename = path.basename(filePath, '.png');
             //用于处理性别标识
-            var resultOfParseItemEmojiCode = [];
+            const resultOfParseItemEmojiCode = [];
             //原始表情图片文件名
-            var partsOfItemEmojiCode = [];
+            let partsOfItemEmojiCode = [];
             if (basename.indexOf('-') > -1) {
                 partsOfItemEmojiCode = basename.split('-');
                 partsOfItemEmojiCode.forEach(function (item) {
@@ -36,24 +36,24 @@ var path = require('path');
             }
 
             emojiPNGFileNameList[convertUTF32MToUTF16m(resultOfParseItemEmojiCode.join('-').replace(/\-/ig, ' '))] = basename;
-            totalCount++;
+            totalCount += 1;
         }
     }
 
     //按最长匹配降序
-    var keysList = Object.keys(emojiPNGFileNameList);
+    let keysList = Object.keys(emojiPNGFileNameList);
     keysList = keysList.sort(function (a, b) {
         return b.length - a.length;
     });
 
-    for (var x = 0; x < keysList.length; x++) {
+    for (let x = 0; x < keysList.length; x++) {
         emojiMapResult[keysList[x]] = emojiPNGFileNameList[keysList[x]];
     }
 
-    var newEmojiMap = {};
+    const newEmojiMap = {};
 
-    for (var k in emojiMapResult) {
-        var item = emojiMapResult[k];
+    for (const k in emojiMapResult) {
+        const item = emojiMapResult[k];
         //需要对表情key进行escape处理,以便与最终文本进行匹配
         newEmojiMap[escape(k).replace(/%u/ig, '\\u')] = item;
     }
@@ -61,4 +61,4 @@ var path = require('path');
     fs.writeFileSync("./map.json", JSON.stringify(newEmojiMap).replace(/\\\\u/ig, '\\u').replace(/%/ig, '\\u00').toLowerCase());
 
     console.log('已成功处理表情文件: ' + totalCount + '个');
-})('./emoji_png')
+})('./emoji_png');
